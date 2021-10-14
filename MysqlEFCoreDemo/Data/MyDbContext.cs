fc5ed34fe1a8a594c36c8449bdc27abaa6c62952
Madhura <madhura.bhujbal@and.digital>
2021-10-14 22:00:54 +0100

    Create new entity class for persons

---
+++ MysqlEFCoreDemo/Data/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MysqlEFCoreDemo.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
---
+++ MysqlEFCoreDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MysqlEFCoreDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
